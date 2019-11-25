39d58b9f964bc10765ffa447d9204df34ec4a5ae
liujun <13381035404@163.com>
2019-11-25 18:07:00 +0800

    no message

--- src/routes/MusicPlay/index.tsx
+++ src/routes/MusicPlay/index.tsx
@@ -3,12 +3,12 @@ import { changeTime } from '@src/utilities/changeTime';
 import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { CircleIcon } from '@src/components/RunIcon/index';
-import changpianIcon from '@src/images/changpian.jpg';
+import changpianIcon from '../../images/changpian.jpg';
 import actions, { getSongDetail, getPlaySongGeci, getPlaySongInfo } from '../../actions/music';
 import { Icon } from 'antd';
-import onlySong from '@src/images/only.png';
-import sortSong from '@src/images/sort.png';
-import xunhuanSong from '@src/images/xunhuan.png';
+import onlySong from '../../images/only.png';
+import sortSong from '../../images/sort.png';
+import xunhuanSong from '../../images/xunhuan.png';
 import SongList from "./components/songList";
 
 import './index.less';
