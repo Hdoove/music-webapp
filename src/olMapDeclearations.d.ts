353d5881b8b1f4dc8f4646cbee31f4031e65ab66
liujun <13381035404@163.com>
2019-11-25 17:57:41 +0800

    img

--- src/olMapDeclearations.d.ts
+++ src/olMapDeclearations.d.ts
@@ -1,23 +1,8 @@
 import { Components } from '@xingheng/stencil-openlayers/dist/types';
 
 declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'ol-map': any,
-            'ol-tile-layer': any,
-            'ol-vector-layer': any,
-            'ol-vector-layer-feature': any,
-            'ol-overlay': any,
-            'ol-base-layer': any,
-            'ol-control-zoom': any,
-            'ol-control-center': any,
-            'ol-control-fullscreen': any,
-            'ol-control-baseswitch': any,
-            'ol-control-mouse-position': any,
-            'ol-control-scaleline': any
-        }
-    }
     interface Window {
         print: any
     }
 }
+declare module '*.png'
